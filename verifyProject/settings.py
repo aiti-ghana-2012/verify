96d2bcb42731a2b544f31b46f25b45378a48e098
AITI Student <aiti-student@aiti-085-PC.(none)>
2012-07-23 15:32:41 +0000

    Users Created and Admin working

--- verifyProject/settings.py
+++ verifyProject/settings.py
@@ -117,6 +117,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'verify',
+    'django.contrib.admin',
+
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
