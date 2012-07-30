c9cc17972af8a71b0eea6ef17623b45fd1ffded8
dakbill <dakbill@yahoo.com>
2012-07-30 10:11:28 +0000

    heroku refresh

--- verify/models.py
+++ verify/models.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 
 class Drug(models.Model):
 	product_name = models.CharField(max_length=300)
-	Generic_name = models.CharField(max_length=300,blank=True)
+	Generic_name = models.TextField(max_length=300,blank=True)
 	strength = models.CharField(max_length=30,blank=True)
 	dosage_form = models.CharField(max_length=30,blank=True)
 	applicant= models.CharField(max_length=100,blank=True)
-	manufacturer = models.CharField(max_length=100,blank=True)
-	local_agent = models.CharField(max_length=100,blank=True)
+	manufacturer = models.TextField(max_length=300,blank=True)
+	local_agent = models.TextField(max_length=300,blank=True)
 	expiry_date = models.CharField(max_length=10,blank=True)
 	
 	
@@ -18,9 +18,9 @@ class Drug(models.Model):
 	
 	
 class Food_water(models.Model):
-	product_name = models.CharField(max_length=60)
-	FDB_number = models.CharField(max_length=15,blank=True)
-	manu_location = models.CharField(max_length=100,blank=True)
+	product_name = models.TextField(max_length=500)
+	FDB_number = models.TextField(max_length=15,blank=True)
+	manu_location = models.TextField(max_length=300,blank=True)
 	dosage_form = models.CharField(max_length=30,blank=True)
 	
 	def __unicode__(self):
